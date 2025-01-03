804dbf7d4c579183425052389a025ee4027243eb
Jeremy <jeremyrwoolley+git@gmail.com>
2025-01-02 20:13:14 -0500

    remove example content

--- src/entrypoints/popup/App.svelte
+++ src/entrypoints/popup/App.svelte
@@ -1,40 +1,15 @@
-<script lang="ts">
-  import svelteLogo from '../../assets/svelte.svg'
-  import Counter from '../../lib/Counter.svelte'
-</script>
-
 <main>
-  <div>
-    <a href="https://wxt.dev" target="_blank" rel="noreferrer">
-      <img src="/wxt.svg" class="logo" alt="WXT Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>WXT + Svelte</h1>
+  <h1>hello world</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p class="read-the-docs">Click on the WXT and Svelte logos to learn more</p>
+  <p>this is the popup</p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  h1 {
+    margin-block: 0.5rem;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #54bc4ae0);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  main {
+    width: max-content;
+    margin: 1rem;
   }
 </style>
--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -1,6 +1,6 @@
 import { mount } from 'svelte'
 import App from './App.svelte'
-import './app.css'
+import '../../lib/base.css'
 
 const app = mount(App, {
   target: document.getElementById('app')!,
