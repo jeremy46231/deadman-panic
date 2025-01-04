357642a2e5e72ba29d2c51c7f338e2237ea7ab99
Saahil <neon@saahild.com>
2025-01-03 20:14:13 -0500

    feat: windowBlur

--- src/lib/triggers/index.ts
+++ src/lib/triggers/index.ts
@@ -1,7 +1,8 @@
 import { KeyboardShortcutTrigger } from './KeyboardShortcutTrigger'
+import { WindowBlur } from './onWindowBlur'
 import { Trigger } from './Trigger'
 
-export const allTriggers = [new KeyboardShortcutTrigger()] satisfies Trigger[]
+export const allTriggers = [new KeyboardShortcutTrigger(), new WindowBlur()] satisfies Trigger[]
 
 export async function setupTriggers(callback: () => void) {
   for (const trigger of allTriggers) {
